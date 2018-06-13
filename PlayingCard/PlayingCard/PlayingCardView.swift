ad5aa58c1baa5758107f466506678cd93b530b29
Sartaj Singh <sartajsingh@Sartajs-MacBook-Pro.local>
2018-06-13 18:34:43 +0530

    gitignore

--- PlayingCard/PlayingCard/PlayingCardView.swift
+++ PlayingCard/PlayingCard/PlayingCardView.swift
@@ -26,7 +26,7 @@ class PlayingCardView: UIView {
         //commenting Bazier Path
         
         let pathBazier = UIBezierPath()
-        pathBazier.addArc(withCenter: CGPoint(x: bounds.midX, y: bounds.midY), radius: 100.0, startAngle: 0, endAngle: 2*CGFloat.pi, clockwise: true);
+        pathBazier.addArc(withCenter: CGPoint(x: bounds.midX, y: bounds.midY), radius: 100.0, startAngle: 0, endAngle: 2*CGFloat.pi, clockwise: true); 
 //        path.lineWidth = 5.0
 //        UIColor.green.setFill()
 //        UIColor.red.setStroke()
